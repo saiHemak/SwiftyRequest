ad085f7f07f15757adb2e6bca6f938ca91c33c72
Aaron Liberatore <AaronLiberatore@aaron.austin.ibm.com>
2018-01-12 15:20:46 -0600

    Adds print

--- Sources/SwiftyRequest/RestRequest.swift
+++ Sources/SwiftyRequest/RestRequest.swift
@@ -189,6 +189,9 @@ public class RestRequest {
             breaker.run(commandArgs: completionHandler, fallbackArgs: "Circuit is open")
         } else {
             let task = session.dataTask(with: request) { (data, response, error) in
+                print("Data", data ?? "")
+                print("Response", response ?? "")
+                print("Error", error ?? "")
                 guard error == nil, let response = response as? HTTPURLResponse else {
                     completionHandler(nil, nil, error)
                     return
